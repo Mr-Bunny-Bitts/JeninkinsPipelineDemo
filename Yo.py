fa96998c494f8e7afaf882f54c8715a09ac5958f
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-13 18:33:54 +0530

    Update Yo.py

--- Yo.py
+++ Yo.py
@@ -2,7 +2,7 @@ import os
 import re
 
 filepath = os.getcwd()
-files = os.listdir(D:\JenkinsHome\workspace\PipelineOne\[REALEASE])
+files = os.listdir("D:\JenkinsHome\workspace\PipelineOne\[REALEASE]")
 files = sorted(files,key=os.path.getmtime)
 for file in files[:11]:
     os.remove(file)
