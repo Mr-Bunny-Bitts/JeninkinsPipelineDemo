8de159dacb62fa9fb53d5568b0813f2fe16934c3
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-13 22:59:58 +0530

    Update Yo.py

--- Yo.py
+++ Yo.py
@@ -1,6 +1,16 @@
-from random import sample
 import os
+APP_FOLDER = 'D:\JenkinsHome\workspace\PipelineOne\[REALEASE]'
 
-files = os.listdir('D:\\JenkinsHome\\workspace\\PipelineOne\\[REALEASE]\\*.zip')
-for file in sample(files,11):
-    os.remove(file)
+totalFiles = 0
+totalDir = 0
+
+for base, dirs, files in os.walk(APP_FOLDER):
+    print('Searching in : ',base)
+    for directories in dirs:
+        totalDir += 1
+    for Files in files:
+        totalFiles += 1
+        
+print('Total number of files',totalFiles)
+print('Total Number of directories',totalDir)
+print('Total:',(totalDir + totalFiles))
