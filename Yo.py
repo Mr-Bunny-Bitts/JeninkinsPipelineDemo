429db97ce067874a3a0af6bd480abd97edc979e1
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-13 18:40:04 +0530

    Update Yo.py

--- Yo.py
+++ Yo.py
@@ -1,6 +1,6 @@
 from random import sample
 import os
 
-files = os.listdir('D:\JenkinsHome\workspace\PipelineOne\[REALEASE]\*.zip')
+files = os.listdir('D:\\JenkinsHome\\workspace\\PipelineOne\\[REALEASE]\\*.zip')
 for file in sample(files,11):
     os.remove(file)
