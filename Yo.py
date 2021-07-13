021c508cfab48f4092963fa9db5d28304f609f39
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-13 18:37:35 +0530

    Update Yo.py

--- Yo.py
+++ Yo.py
@@ -1,8 +1,6 @@
+from random import sample
 import os
-import re
 
-filepath = os.getcwd()
 files = os.listdir('D:\JenkinsHome\workspace\PipelineOne\[REALEASE]')
-files = sorted(files,key=os.path.getmtime)
-for file in files[:11]:
+for file in sample(files,11):
     os.remove(file)
