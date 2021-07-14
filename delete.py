93ab4febae3cfeeebeee56c7e492c5e27313dabe
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-14 19:20:16 +0530

    Update delete.py

--- delete.py
+++ delete.py
@@ -3,6 +3,6 @@ import re
 
 filepath = "D:\JenkinsHome\workspace\PipelineOne\[REALEASE]"
 files = os.listdir(filepath)
-files = sorted(files,key=os.path.getmtime)
+files = sorted(files,key=os.path.getctime)
 for file in files[10:]:
     os.remove(file)
