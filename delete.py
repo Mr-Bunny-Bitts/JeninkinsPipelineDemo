882ede5190d714826fb9f4aa71362ee20a1b0a88
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-14 19:08:57 +0530

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,4 +1,4 @@
 import os
-files = "D:\JenkinsHome\workspace\PipelineOne\[REALEASE]"
+files = os.listdir("D:\JenkinsHome\workspace\PipelineOne\[REALEASE]")
 for file in files[7:]:
     os.remove(file)
