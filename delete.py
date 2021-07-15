c3e5e2a1871b110410ac7d2ffcaf0946362115ab
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-07-15 23:40:55 +0530

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,8 +1,18 @@
 import os
 import re
 
-filepath = "D:\JenkinsHome\workspace\PipelineOne\[REALEASE]"
+filepath = "D:\\JenkinsHome\\workspace\\PipelineOne\\[REALEASE]\\"
 files = os.listdir(filepath)
-sorted_files = sorted(files)
-for file in sorted_files[10:]:
-    os.remove(file)
+
+sorted_files = sorted(files,key=os.path.getmtime, reverse=False)
+
+
+for files in (sorted_files):
+	
+	os.remove(files)
+	files1 = os.listdir(filepath)
+	sorted_files = sorted(files1,key=os.path.getmtime, reverse=False)
+	count=len(sorted_files)
+	print (count)
+	if count <=10:
+		break;
