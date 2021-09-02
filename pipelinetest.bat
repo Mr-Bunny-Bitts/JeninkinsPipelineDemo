1f51d5a44ef990f4f96e4cf2659a587186c01e1b
Shivam Vishwakarma <76660253+Mr-Bunny-Bitts@users.noreply.github.com>
2021-09-02 11:18:41 +0530

    Update pipelinetest.bat

--- pipelinetest.bat
+++ pipelinetest.bat
@@ -3,5 +3,5 @@ set data=%d%
 for /f "tokens=1,2 delims=:. " %%x in ("%time%") do set t=%%x%%y
 set time=%t%
 Echo zipping...
-"C:\\Program Files\\7-Zip\\7z.exe" a -tzip "D:\\JenkinsHome\\workspace\\PipelineOne\\Test_Zipping_%d%_%t%.zip" "D:\\JenkinsHome\\workspace\\PipelineOne\\Realease_Timestamp"
+"C:\\Program Files\\7-Zip\\7z.exe" a -tzip "D:\\JenkinsHome\\workspace\\test_pipeline1\\Test_Zipping_%d%_%t%.zip" "D:\\JenkinsHome\\workspace\\PipelineOne\\Realease_Timestamp"
 echo Done
